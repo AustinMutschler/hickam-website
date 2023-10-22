c8378a26dbcfeeed5ff0ec8fa215401dc5da96da
Austin Mutschler <austin@austins-mbp.mynetworksettings.com>
2023-10-22 12:20:38 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hickam.com
+
+This is the repository for a family website https://hickam.com
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div id="main-page">
+      <h1>App</h1>
+    </div>
+  );
+}
+
+export default App;
